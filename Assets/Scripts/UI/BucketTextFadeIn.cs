b24b76c949c953203d4e2b9c92572d81c59d8c85
Slimesauce <nicholasmania0@gmail.com>
2025-05-07 22:27:26 -0400

    Added bucket fade-in text

--- Assets/Scripts/UI/BucketTextFadeIn.cs
+++ Assets/Scripts/UI/BucketTextFadeIn.cs
@@ -12,15 +12,15 @@ public class BucketTextFadeIn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.transform.localScale = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!enemyManager.waveActive && enemyManager.waveCounter == 9)
+        if ((!enemyManager.waveActive && enemyManager.numEnemiesLeft == 0) && enemyManager.waveCounter == 9)
         {
-            this.transform.localScale = Vector3.one;
+            this.transform.localScale = new Vector3(9.02139f, 2.62378f, 3.08434f);
         }
         else
         {
